8d5490693e5e321b0e9bf0d4410a111f6ccec38b
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-27 12:48:25 -0400

    devise funcionando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+  
+  
+    def after_sign_in_path_for(resource)
+      root_path
+    end
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :rut_number])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :rut_number, :role])
+    end
+  end
