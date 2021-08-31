1cfeae1b93396277068dc613fe1faeacdcaf9f0b
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-31 11:58:43 -0400

    agregando los primeros routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :pages, only: [:about, :home]
+  get 'about', to: 'pages#about'
   resources :products, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
