d9a30cfce5371970b91a32fb4fe3dd60186e43ed
paloma anriquez <paloma.seguel93@gmail.com>
2021-09-06 01:02:39 -0300

    cambio de estructura de card product y loop de visualización

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ Product.destroy_all
 puts "Creando mi primer producto"
 Product.create(title:"6 Donas sin lactosa", price:6000, description:"apto para celiacos")
 puts "Creado mi primer producto"
+
+puts "Creando mi segungo producto"
+Product.create(title:"10 bombones de chocolate", price:2000, description:"apto para celiacos y sin lactosa")
+puts "Creado mi segundo producto"
