609d37d5d51d90c6c21aedb01b6dace03082c4eb
jorge pilcante <jorgeapd5@hotmail.com>
2021-09-07 00:08:44 -0300

    agregando botones y centrando sign in

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,15 @@
 class CartsController < ApplicationController
     skip_before_action :authenticate_user!, only: [:create, :show, :edit]
-    def show
-    end
-  
+    
     def create
+      product = Product.find(params[:id])
+      @cart = Cart.new params[:product]
+      @cart.product = product
+      @cart.save
+      redirect_to cart_path(@cart)
     end
-  
-    def edit
+    
+    def show 
+      @cart = Cart.find(params[:id])
     end
 end
